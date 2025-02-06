f697eebc0099fc69bcf18506a09218207cf393ae
Okeke Chioma Princess <okekegeraldenne@gmail.com>
2025-02-06 23:11:32 +0100

    user authentication done

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,4 @@
+const generateToken = require("../jwt/tokengenerator");
 const userModel = require("../model/userModel");
 const bcrypt = require("bcryptjs");
 
@@ -16,29 +17,66 @@ const register = async (req, res, next) => {
     try {
         const user = await userModel.findOne({ email });
         if (user) {
-            res.status(400).json({msg: "User already exists"})
+            res.status(400).json({ msg: "User already exists" });
         }
 
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password, salt)
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
         const newUser = new userModel({
             ...req.body,
-            password: hashedPassword
-        })
+            password: hashedPassword,
+        });
 
-        await newUser.save()
-        res.status(200).json(newUser)
-
+        await newUser.save();
+        res.status(200).json(newUser);
     } catch (error) {
         next({ status: 500, message: "SOmething went wrong" });
     }
 };
 
- const login = async (req, res, next) => {
-    const {email, password} = req.body
+const login = async (req, res, next) => {
+    const { email, password } = req.body;
 
-    trycatch
- }
+    try {
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return res.status(404).json({
+                msg: "No existing user found with that email address",
+            });
+        }
 
-module.exports = {register}
+        const comparePassword = await bcrypt.compare(password, user.password);
+        if (!comparePassword) {
+            return (
+                res.status(400).json *
+                { msg: "email address or password incorrect" }
+            );
+        }
+
+        const { password: _, ...userData } = user.toObject();
+        const token = generateToken(user._id);
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: 3600000,
+        });
+        res.status(200).json({ msg: "User logged in", user: userData });
+    } catch (error) {
+        next({ status: 500, message: "Something went wrong" });
+    }
+};
+
+const logout = async (req, res, next) => {
+    res.cookie('token', "", {
+        httpOnly: true,
+        secure: process.env.NOD_ENV === "production",
+        sameSite: "strict",
+        maxAge: 0
+    })
+    res.status(200).json({msg: "User successfully logged out"})
+}
+
+module.exports = { register, login, logout };
--- router/authRouter.js
+++ router/authRouter.js
@@ -1,9 +1,9 @@
 const { Router } = require("express");
-const { register } = require("../controller/userController");
+const { register, login, logout } = require("../controller/userController");
 
 const authRouter = Router()
     .post("/auth/register", register)
-    .post("/auth/login")
-    .post("/auth/logout");
+    .post("/auth/login", login)
+    .post("/auth/logout", logout);
 
 module.exports = authRouter;
